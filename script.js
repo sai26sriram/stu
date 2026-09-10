491ec1e8239bd533c3f53f5adc9eacb656c2758e
Sai Sriram B S <saisrirambalasubramani@gmail.com>
2026-09-10 10:44:01 +0530

    updated 3 files

---
+++ script.js
@@ -0,0 +1,16 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
